b0c871bf5f29077fbc05497492a8ea7178846645
Quentin Jin <jianstm@gmail.com>
2019-04-03 17:43:06 +0800

    Update all manifests

--- Package.swift
+++ Package.swift
@@ -1,16 +1,23 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "Schedule",
+    platforms: [
+        .macOS(.v10_12),
+        .iOS(.v10),
+        .tvOS(.v10),
+        .watchOS(.v3)
+    ],
     products: [
-        .library(name: "Schedule", targets: ["Schedule"]),
-        .executable(name: "ScheduleDemo", targets: ["ScheduleDemo"])
+        .library(name: "Schedule", targets: ["Schedule"])
     ],
     targets: [
         .target(name: "Schedule"),
-        .testTarget(name: "ScheduleTests", dependencies: ["Schedule"]),
-        .target(name: "ScheduleDemo", dependencies: ["Schedule"])
+        .testTarget(name: "ScheduleTests", dependencies: ["Schedule"])
+    ],
+    swiftLanguageVersions: [
+        .v5
     ]
 )
--- Schedule.podspec
+++ Schedule.podspec
@@ -1,15 +1,15 @@
 Pod::Spec.new do |s|
   s.name             = "Schedule"
-  s.version          = "2.0.0-beta.1"
+  s.version          = "2.0.0"
   s.license          = { :type => "MIT" }
   s.homepage         = "https://github.com/jianstm/Schedule"
   s.author           = { "Quentin Jin" => "jianstm@gmail.com" }
-  s.summary          = "Lightweight timing task scheduler"
+  s.summary          = "Schedule timing task in Swift using a fluent API"
 
   s.source           = { :git => "https://github.com/jianstm/Schedule.git", :tag => "#{s.version}" }
   s.source_files     = "Sources/Schedule/*.swift"
   
-  s.swift_version    = "4.2"
+  s.swift_version    = "5.0"
 
   s.ios.deployment_target = "10.0"
   s.osx.deployment_target = "10.12"
